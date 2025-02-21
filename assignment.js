8cd76de132f21732bd3804b675b06f6ef4fb8457
khansufyaan315 <khansufyaan315@gmail.com>
2025-02-22 02:43:08 +0530

    Task4

---
+++ assignment.js
@@ -0,0 +1,10 @@
+function Person(){
+    var name = nm;
+    var age = a;
+    var gender = gend;
+    var greet = function(){
+        console.log("Hello, I am "+name);
+    };
+};
+
+let person1 = new Person();
